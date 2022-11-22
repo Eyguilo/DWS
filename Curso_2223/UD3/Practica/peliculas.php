c559b9dcc3889ef5d5fce4fcab5ccab1f04fba6d
jaume_aguilo <jaguilopiza@iessonferrer.net>
2022-11-22 20:57:22 +0100

    22/11/22 - Soy la ostia.

--- Curso_2223/UD3/Practica/peliculas.php
+++ Curso_2223/UD3/Practica/peliculas.php
@@ -15,76 +15,66 @@ if($categoria == "terror"){
 }
 
 echo"
-
     <meta charset='UTF-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <link rel='stylesheet' href='".$estilo.".css'>
-    <title>Películas</title>
+    <title>Películas: ".$categoria."</title>
 </head>
-<body>
-"
+<body>";
 ?>
 
 <?php
-    echo "<div class='contenedor'>
 
-        <div class='primera_caja'>
-            <h1 class='titulo_categoria'>Categoría: </h1>
+    echo "<div class='contenedor'>";
+
+    echo "<div class='primera_caja'>
+            <h1 class='titulo_categoria'>Categoría: ".$categoria."</h1>
             <a class='enlace_inicio' href='categorias.php'>Inicio</a>
-        </div>
+        </div>";
 
-        <div class='segunda_caja'>
-            <div class='primera_columna'>
-                <div class='titulo_caja'><h3>El resplandor</h3></div>
-                <div class='imagen_caja'>
-                    <img src='imagenes/the_shining.jpg' alt='imagen_the_shining'>
-                </div>
-                <div class='duracion_caja'>Duración: </div>
-            </div>
-            <div class='segunda_columna'>
-                <div class='votos_caja'>Votos: </div>
-                <div class='sinopsis_caja'>Sinopsis: </div>
-                <div class='enlace_caja'>Enlace: <a class='enlace_ficha' href='fichas.php'>Ver ficha</a></div>
-            </div>
-            <div class='tercera_columna'>
-            </div>
-        </div>
-        
-        <div class='segunda_caja'>
-            <div class='primera_columna'>
-                <div class='titulo_caja'><h3>Insidious</h3></div>
-                <div class='imagen_caja'>
-                    <img src='imagenes/insidious.jpg' alt='imagen_insidious'>
-                </div>
-                <div class='duracion_caja'>Duración: </div>
-            </div>
-            <div class='segunda_columna'>
-                <div class='votos_caja'>Votos: </div>
-                <div class='sinopsis_caja'>Sinopsis: </div>
-                <div class='enlace_caja'>Enlace: </div>
-            </div>
-            <div class='tercera_columna'>
-            </div>
-        </div>
+    class Pelicula{
 
-        <div class='segunda_caja'>
-            <div class='primera_columna'>
-                <div class='titulo_caja'><h3>Insidious</h3></div>
-                <div class='imagen_caja'>
-                    <img src='imagenes/insidious.jpg' alt='imagen_insidious'>
-                </div>
-                <div class='duracion_caja'>Duración: </div>
-            </div>
-            <div class='segunda_columna'>
-                <div class='votos_caja'>Votos: </div>
-                <div class='sinopsis_caja'>Sinopsis: </div>
-                <div class='enlace_caja'>Enlace: </div>
-            </div>
-            <div class='tercera_columna'>
-            </div>
-        </div>
-    </div>"
+        private $titulo;
+        private $imagen;
+        private $duracion;
+        private $votos;
+
+        function __constructor($titulo, $imagen, $dureacion, $votos){
+            $this->titulo = $titulo;
+            $this->imagen = $imagen;
+            $this->duracion = $duracion;
+            $this->votos = $votos;
+        }
+
+        function mostrarPelicula(){
+
+            for ($i=0; $i < 1; $i++) {             
+
+                echo "
+                <div class='segunda_caja'>
+                    <div class='primera_columna'>
+                        <div class='titulo_caja'><h3>".$titulo."</h3></div>
+                        <div class='imagen_caja'>
+                            <img src='".$imagen."' alt='imagen_the_shining'>
+                        </div>
+                        <div class='duracion_caja'>Duración: ".$duracion." min.</div>
+                    </div>
+                    <div class='segunda_columna'>
+                        <div class='votos_caja'>Votos: ".$votos."</div>
+                        <div class='sinopsis_caja'>Sinopsis: </div>
+                        <div class='enlace_caja'>Enlace: <a class='enlace_ficha' href='fichas.php'>Ver ficha</a></div>
+                    </div>
+                    <div class='tercera_columna'></div>
+                </div>";
+            }
+        }
+    }
+
+    $pelicula1 = new Pelicula ("El Resplandor", "imagenes/the_shining.jpg", 103, 5);
+    $pelicula1->mostrarPelicula();                               
+    
+    echo "</div>";
 ?>      
 </body>
 </html>
