1744771df702fdcb62286d51b5b89292936400e4
jaume_aguilo <jaguilopiza@iessonferrer.net>
2022-11-29 20:53:46 +0100

    29/11/22 - Se ha adelantado un montón :).

--- Curso_2223/UD3/Practica/prueba.php
+++ Curso_2223/UD3/Practica/prueba.php
@@ -1,38 +1,92 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang='es'>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prueba</title>
-</head>
-<body>
+
+    <?php 
+
+    $estilo;
+    $categoria = $_GET['categoria'];
+
+    if($categoria == "terror"){
+        $id_categoria = 1;
+    }elseif($categoria == "anime"){
+        $id_categoria = 2;
+    }
+
+    echo "
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link rel='stylesheet' href='css/estilos_".$categoria.".css'>
+        <title>Películas: ".$categoria."</title>
+    </head>
+    <body>";
+    ?>
 
     <?php
 
-        ini_set('display_errors', 1);
-        ini_set('html_errors', 1);
+    ini_set('display_errors', 1);
+    ini_set('html_errors', 1);
 
-        $conexion = mysqli_connect('localhost', 'root', '1234');
+    echo "<div class='contenedor'>";
 
-        mysqli_select_db($conexion, 'cartelera');
+    echo "<div class='primera_caja'>
+            <h1 class='titulo_categoria'>Categoría: ".$categoria."</h1>
+            <a class='enlace_inicio' href='categorias.php'>Inicio</a>
+        </div>";
 
-        $consulta = "SELECT * FROM T_Pelicula;";
 
-        $resultado = mysqli_query($conexion, $consulta);
 
+
+    function mostrarPelicula($id_categoria, $categoria){
+
+    $conexion = mysqli_connect('localhost', 'root', '1234');
+    mysqli_select_db($conexion, 'cartelera');
+    $consulta = "SELECT * FROM T_Pelicula WHERE id_categoria = $id_categoria;";
+    $resultado = mysqli_query($conexion, $consulta);
+    
         if(!$resultado){
+
             $mensaje = 'Consulta inválida: '.mysqli_error($conexion)."\n";
-            $mensaje = 'Consulta realitzada: '.$consulta;
+            $mensaje .= 'Consulta realitzada: '.$consulta;
             die($mensaje);
+
         } else{
 
-            echo "Conexión OK."."<br><br>";
+            
             while($registro = mysqli_fetch_assoc($resultado)){
 
-                echo $registro['titulo']."<br><br>";
+                echo (var_dump($imagen = $registro['imagen']));
+
+                echo "
+                <div class='segunda_caja'>
+                    <div class='primera_columna'>
+                        <div class='titulo_caja'><h3>".$titulo = $registro['titulo']."</h3></div>
+                        <div class='imagen_caja'>
+                            <img src='imagenes/".$categoria."/".$imagen = $registro['imagen']."' alt='".$imagen = $registro['imagen']."'>
+                        </div>
+                        <div class='duracion_caja'>Duración: ".$duracion = $registro['duracion']." min.</div>
+                    </div>
+                    <div class='segunda_columna'>
+                    <div class='votos_caja'>Votos: ".$votos = $registro['votos']."</div>
+                    <div class='sinopsis_caja'>".longitudSinopsis($sinopsis = $registro['sinopsis'])."<a class='enlace_ficha' href='ficha.php'>...</a></div>
+                    <div class='enlace_caja'>Enlace: <a class='enlace_ficha' href='ficha.php?id=".$id = $registro['id']."'>Ver ficha</a></div>
+                </div>
+                <div class='tercera_columna'></div>
+                </div>"; 
             }
-        }
-    ?> 
+        }  
+    }
+    
+    function longitudSinopsis($sinopsis){
+        $resumen = substr($sinopsis, 0, 200);
+        return $resumen;        
+    }
+
+    mostrarPelicula($id_categoria, $categoria);
+
+    echo "</div>";
+
+    ?>      
 </body>
 </html>
