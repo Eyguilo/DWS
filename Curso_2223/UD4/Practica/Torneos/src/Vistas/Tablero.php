26787b2ff18c2e04d8154ccd18914e9112108017
jaume_aguilo <jaguilopiza@iessonferrer.net>
2023-01-13 18:17:58 +0100

    13/01/23 - Se ha adelantado maquetación.

---
+++ Curso_2223/UD4/Practica/Torneos/src/Vistas/Tablero.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tablero</title>
+    <link rel="stylesheet" href="../../css/tablero.css">
+</head>
+<body>
+    <div class="contenedor">
+        <div class="caja1"><h1>Mundial de Tenis de Mesa</h1></div>
+        <div class="caja2">
+            <div class="col1"></div>
+            <div class="col2">
+                <h2>CUARTOS</h2>
+                <div class="parejasC">
+                    <div class="jugadorC">Charles Negro</div>
+                    <div class="jugadorC">Ylijendri Canas</div>
+                </div>
+                <div class="parejasC">
+                    <div class="jugadorC">Fernidio Mecagoendia</div>
+                    <div class="jugadorC">Marai Motos</div>
+                </div>
+                <div class="parejasC">
+                    <div class="jugadorC">Lerdian Ganut</div>
+                    <div class="jugadorC">Lucio Okolo</div>
+                </div>
+                <div class="parejasC">
+                    <div class="jugadorC">Jorge Agüila</div>
+                    <div class="jugadorC">Ghizermo Quesiii</div>
+                </div>                
+            </div>
+            <div class="col1"></div>
+            <div class="col2">
+                <h2>SIMIFINALES</h2>
+                <div class="parejasSF">
+                    <div class="jugadorSF">Charles Negro</div>
+                    <div class="jugadorSF">Fernidio Mecagoendia</div>
+                </div>
+                <div class="parejasSF">
+                    <div class="jugadorSF">Lucio Okolo</div>
+                    <div class="jugadorSF">Ghizermo Quesiii</div>
+                </div>
+            </div>
+            <div class="col1"></div>
+            <div class="col2">
+                <h2>FINAL</h2>
+                <div class="parejasF">
+                    <div class="jugadorF">Charles Negro</div>
+                    <div class="jugadorF">Ghizermo Quesiii</div>
+                </div>
+            </div>
+            <div class="col1"></div>
+            <div class="col2">
+                <h2>CAMPEÓN</h2>
+                <div class="jugadorG">Charles Negro</div>
+            </div>
+            <div class="col1"></div>
+            <div class="imagen"></div>
+        </div>
+    </div>
+    
+</body>
+</html>
