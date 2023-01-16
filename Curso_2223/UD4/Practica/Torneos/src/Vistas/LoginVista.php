37514a9ad51d11c40b34feaf64fdf3e1858415ae
Eyguilo <jaumeeltoro@gmail.com>
2023-01-16 03:54:37 +0100

    16/01/23 - Se ha finalizado la maquetación de Inicio de Sesión.

--- Curso_2223/UD4/Practica/Torneos/src/Vistas/LoginVista.php
+++ Curso_2223/UD4/Practica/Torneos/src/Vistas/LoginVista.php
@@ -5,9 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar sesión</title>
-    <link rel="stylesheet" href="LoginVista.css">
+    <link rel="stylesheet" href="../../css/LoginVista.css">
 </head>
 <body>
-    
+    <div id="contenedor">
+        <div id="central">
+            <div id="login">
+                <div class="titulo">Bienvenido</div>
+                <form id="loginform">
+                    <input type="text" name="usuario" placeholder="Usuario" required>
+                    <input type="password" placeholder="Contraseña" name="password" required>
+                    <button type="submit" title="Ingresar" name="Ingresar">Iniciar</button>
+                </form>
+                <div class="pie-form">
+                    <a href="#">¿Perdiste tu contraseña?</a>
+                    <a href="#">¿No tienes cuenta? Registrate</a>
+                </div>
+            </div>
+            <div class="inferior">
+                <a href="#">Volver</a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
