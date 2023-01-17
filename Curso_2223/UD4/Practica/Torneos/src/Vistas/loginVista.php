7de6839b7da69e9188fd422352e6682118d4a67a
jaume_aguilo <jaguilopiza@iessonferrer.net>
2023-01-17 20:49:05 +0100

    17/01/23 - Se ha hecho más bien poco.

--- Curso_2223/UD4/Practica/Torneos/src/Vistas/loginVista.php
+++ Curso_2223/UD4/Practica/Torneos/src/Vistas/loginVista.php
@@ -5,21 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar sesión</title>
-    <link rel="stylesheet" href="../../css/LoginVista.css">
+    <link rel="stylesheet" href="../../css/loginVista.css">
 </head>
 <body>
     <div id="contenedor">
         <div id="central">
-            <div id="login">
+            <div id="inicio">
                 <div class="titulo">Bienvenido</div>
                 <form id="loginform">
                     <input type="text" name="usuario" placeholder="Usuario" required>
                     <input type="password" placeholder="Contraseña" name="password" required>
                     <button type="submit" title="Ingresar" name="Ingresar">Iniciar</button>
                 </form>
-                <div class="pie-form">
+                <div class="pie">
                     <a href="#">¿Perdiste tu contraseña?</a>
-                    <a href="#">¿No tienes cuenta? Registrate</a>
+                    <a href="#">¿No tienes cuenta? Regístrate</a>
                 </div>
             </div>
             <div class="inferior">
